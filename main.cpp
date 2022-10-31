429461697b44b83ede5a524de0d97b8e4ea5ac7f
wzjaroszynski <112191003+wzjaroszynski@users.noreply.github.com>
2022-10-31 23:11:23 +0000

    for loop written

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int main()
  int total = 0, *nPt;
   
  nPt = nums; // store address of nums[0] in nPt
- while (nPt < nums + NUMS)
- total += *nPt++;
+
+for ( int i = 0; i <= NUMS; i++){
+  total += *nPt++;
  cout << "The total of the array elements is " << total << endl;
  return 0;
+  }
   }
